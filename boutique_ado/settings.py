8c40ce26aa54a3972802b4d47217b8e30cea4748
Jordan Cooper <jordan_cooper@outlook.com>
2024-04-03 12:51:02 +0000

    Added Home app and templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'home',
 
     'allauth',
     'allauth.account',
@@ -63,7 +64,10 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
